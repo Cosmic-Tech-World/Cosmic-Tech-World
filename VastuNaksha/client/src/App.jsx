3933b5f4a8cdd2b9fce8b67df9a5ea88d3fdb599
Punam Prajapati <punamprajapati1012@gmail.com>
2026-01-05 02:17:41 +0530

    fixed check-direction navbar options

--- VastuNaksha/client/src/App.jsx
+++ VastuNaksha/client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import DirectionAnalysis from './pages/DirectionAnalysis';
 import Brochure from './pages/Brochure';
@@ -6,9 +7,31 @@ import Contact from './pages/Contact';
 import AdminPanel from './admin/AdminPanel';
 import './App.css';
 
+const ScrollToAnchor = () => {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (!hash) {
+      window.scrollTo(0, 0);
+    } else {
+      const timer = setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }, 100);
+      return () => clearTimeout(timer);
+    }
+  }, [pathname, hash]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
+      <ScrollToAnchor />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/check-direction" element={<DirectionAnalysis />} />
