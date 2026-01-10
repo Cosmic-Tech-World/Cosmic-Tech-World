5ffd78fbf7b0f646bd9622e5306530813ed789ab
Punam Prajapati <punamprajapati1012@gmail.com>
2026-01-10 22:24:32 +0530

    change email

--- VastuNaksha/client/src/pages/Brochure.jsx
+++ VastuNaksha/client/src/pages/Brochure.jsx
@@ -507,7 +507,7 @@ const Brochure = () => {
                                         <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                         <polyline points="22,6 12,13 2,6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
-                                    <p>contact@vastunaksha.online</p>
+                                    <p>connect@vastunaksha.online</p>
                                 </div>
                                 <div className="footer-contact-item">
                                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
