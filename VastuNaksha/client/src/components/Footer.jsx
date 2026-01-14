e23aaa79f1735242d7028fc519f85ffd5d7bc31a
Punam Prajapati <punamprajapati1012@gmail.com>
2026-01-14 17:56:25 +0530

    add

--- VastuNaksha/client/src/components/Footer.jsx
+++ VastuNaksha/client/src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Footer.css';
 import logo from '../assets/logo.png';
 
+import ctlLogo from '../assets/ctl-logo.png';
+
 const Footer = () => {
     return (
         <footer className="footer">
@@ -14,8 +16,10 @@ const Footer = () => {
                 </div>
                 <div className="footer-copyright">
                     © 2025 Vastunaksha. All rights reserved • Made with care
-                    <br />
-                    A Product by Cosmic Thinking Lab
+                    <div className="ctl-brand-container">
+                        <span>A Product by Cosmic Thinking Lab</span>
+                        <img src={ctlLogo} alt="Cosmic Thinking Lab" className="ctl-logo" />
+                    </div>
                 </div>
                 <div className="footer-links">
                     <a href="#privacy">Privacy</a>
